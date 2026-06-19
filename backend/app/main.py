dbdbc1d9dd8e727702dd4ed706d26205ff34e064
tpercival01 <thomaspercival@live.co.uk>
2026-06-19 12:26:52 +0100

    changed cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://fabricated-v2.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
